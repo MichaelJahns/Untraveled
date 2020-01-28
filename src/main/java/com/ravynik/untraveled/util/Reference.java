1039a82d9a4959813cdc6b81004046a867510eb2
MichaelJahns <jahnsmic001@gmail.com>
2020-01-28 14:05:09 -0800

    a misplaced semicolon was inside of the modid string

--- src/main/java/com/ravynik/untraveled/util/Reference.java
+++ src/main/java/com/ravynik/untraveled/util/Reference.java
@@ -1,7 +1,7 @@
 package com.ravynik.untraveled.util;
 
 public class Reference {
-    public static final String MOD_ID = "untraveledmod;";
+    public static final String MOD_ID = "untraveledmod";
     public static final String NAME = "Untraveled Mod";
     public static final String VERSION = "0.0.00";
     public static final String ACCEPTED_VERSIONS = "[1.14.4]";
